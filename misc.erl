df44ff22faaaeb359a80f2cba7cbad73ecd0a4d9
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2012-02-29 11:44:37 +0400

    Add crawl_ancestors/2 and get_ancestors/1 for creating graph of the ancestors

--- misc.erl
+++ misc.erl
@@ -10,3 +10,42 @@ launchpad() ->
                                       unlink(Pid)
                               end)).
 
+crawl_registered(documentation) ->
+    "Gets ancestors of a registered process (must conform OTP)."
+        " And the ancestors of them recursively."
+        " Starting from registered().".
+crawl_registered() ->
+    crawl_ancestors(registered(), []).
+
+
+%% This code can be used to flatten the graph:
+%% AGF = lists:flatmap(fun ({P,A}) -> lists:map(fun (E) -> {P,E} end, A) end, AG).
+
+crawl_ancestors(documentation) ->
+    "Gets ancestors of all the processes or a process (must conform OTP)."
+        " And the ancestors of them recursively.".
+
+crawl_ancestors(PNames, Links) when is_list(PNames) ->
+    lists:foldl(fun misc:crawl_ancestors/2, Links, PNames);
+crawl_ancestors(PName, Links) -> % when is_atom(PName) ->
+    case proplists:is_defined(PName, Links) of
+        true -> Links;
+        _ ->
+            io:format("PName: ~p~n", [PName]),
+            Ancestors = get_ancestors(PName),
+            Link = proplists:property(PName, Ancestors),
+            crawl_ancestors(Ancestors, [Link | Links])
+    end.
+
+get_ancestors(documentation) ->
+    "Returns ancestors of the process that conform to OTP.";
+get_ancestors(PName) when is_pid(PName) -> [];
+get_ancestors(PName) when is_atom(PName) ->
+    case catch sys:get_status(PName) of
+        {status,_,_,Status} ->
+            Ancestors = proplists:get_value('$ancestors', hd(Status), []),
+            Ancestors;
+        _ -> []
+    end.
+
+
