089215da84ded2428824284ead414fa46c8e0f14
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2012-02-29 13:58:11 +0400

    Add group of simple use cases tests for Config server

--- clustmea_conf_SUITE.erl
+++ clustmea_conf_SUITE.erl
@@ -5,10 +5,15 @@
 -compile(export_all).
 
 groups() ->
-    [{implementation_group, [parallel], [reset_options, set_options]}].
+    [{implementation_group, [parallel],
+      [reset_options,set_options]},
+     {simple_use_cases_group, [sequence],
+      [list_configs,get_config_options]},
+     {parallel_group, [parallel],
+      [{group,implementation_group},{group,simple_use_cases_group}]}].
 
 all() ->
-    [{group,implementation_group}].
+    [{group,parallel_group}].
 
 %%--------------------------------------------------------------------
 %% TEST CASES
@@ -30,6 +35,46 @@ set_options(_Config) ->
             {[[{a,1},{b,2}],[{b,3}]], '=>', [{a,1},{b,3}]}],
     check_fun_on_data(clustmea_conf, set_options, Data).
 
+list_configs(_Config) ->
+    CC = clustmea_conf,
+    {ok, _Pid} = CC:start_link(),
+    {ok, []} = CC:list(),
+    {ok, Cid} = CC:new(),
+    {ok, [Cid]} = CC:list(),
+    ok = CC:delete(Cid),
+    {ok, []} = CC:list().
+
+get_config_options(_Config) ->
+    CC = clustmea_conf,
+    {ok, _Pid} = CC:start_link(),
+    {ok, Cid} = CC:new(),
+    {ok, []} = CC:get(Cid),
+    ok = CC:set(Cid, [{a,1}]),
+    {ok, [{a,1}]} = CC:get(Cid),
+    ok = CC:reset(Cid, [a]),
+    {ok, []} = CC:get(Cid),
+    ok = CC:set(Cid, [{a,2}, {b,b1}]),
+    ok = CC:set(Cid, [{a,3}]),
+    case CC:get(Cid) of
+        {ok, [{a,3}, {b,b1}]} -> ok_1;
+        {ok, [{b,b1}, {a,3}]} -> ok_2
+    end.
+
+%%--------------------------------------------------------------------
+%% HELPER FUNCTIONS
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Check function across data
+%%
+%% @spec check_fun_on_data(M, F, InOutList) -> [M:F(InOut)]
+%%         InOutList -> [InOut]
+%%         InOut -> {In,'=>',Out}
+%%         In -> list()
+%%         Out -> term()
+%% @end
+%%--------------------------------------------------------------------
 check_fun_on_data(documentation) ->
     "Checks function M:F on Data."
         "InOutList -> [InOut]"
@@ -41,3 +86,4 @@ check_fun_on_data(M, F, InOutList) ->
                       Out = apply(M, F, In)
               end,
               InOutList).
+
