718fc4d447ed9762dbc4ff90f2a669fef5012e6d
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2012-03-20 17:01:20 +0400

    Refactor common code, add check_riak_inets/0

--- scratch.erl
+++ scratch.erl
@@ -4,14 +4,36 @@
 
 -include("uploader_conf.hrl").
 
-check1() ->
-    {ok, C} = clustmea_conf:new(),
-    ok = clustmea_conf:set(C, [{uploader_conf,
-                                #uploader_conf{seed=1,
-                                               value_size=5,
-                                               quantity=100,
-                                               connection=xx_connection}},
-                               {iteration, hard}]),
-    {ok, T} = clustmea_task:new(my_uploader),
-    clustmea_task:start_task(T,C),
+check_my_uploader() ->
+    run(worker, my_uploader,
+        on_conf, [{uploader_conf,
+                   #uploader_conf{seed=1,
+                                  value_size=10,
+                                  quantity=200,
+                                  connection=xx_connection}},
+                  {iteration, soft}],
+       and_stop_ASAP).
+
+check_riak_inets() ->
+    run(worker, riak_inets_uploader,
+        on_conf, [{uploader_conf,
+                   #uploader_conf{seed=1,
+                                  value_size=10,
+                                  quantity=20,
+                                  connection=xx_connection}},
+                  {iteration, soft}]).
+
+%%
+%% Internal functions
+%%
+
+run(worker, Worker, on_conf, Conf) ->
+    {ok,C} = clustmea_conf:new(),
+    ok = clustmea_conf:set(C, Conf),
+    {ok,T} = clustmea_task:new(Worker),
+    clustmea_task:start_task(T, C),
+    {ok,T}.
+
+run(worker, Worker, on_conf, Conf, and_stop_ASAP) ->
+    {ok,T} = run(worker, Worker, on_conf, Conf),
     clustmea_task:stop_task(T).
