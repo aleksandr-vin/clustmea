889c540b8b59a8c2df00a7ab46f76873b44111bf
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2012-03-20 17:03:32 +0400

    Add riak_inets_uploader worker module, basic PUT to the localhost RIAK cluster

---
+++ riak_inets_uploader.erl
@@ -0,0 +1,104 @@
+%%%-------------------------------------------------------------------
+%%% @author Aleksandr Vinokurov <aleksandr.vin@gmail.com>
+%%% @copyright (C) 2012
+%%% @doc
+%%% RIAK Uploader through inets worker
+%%% @end
+%%% Created : 20 Mar 2012 by Aleksandr Vinokurov <aleksandr.vin@gmail.com>
+%%%-------------------------------------------------------------------
+-module(riak_inets_uploader).
+
+-behaviour(gen_uploader).
+
+-define(make_server_name, misc:make_ref_atom(?MODULE)).
+
+%% API
+-export([start_link/1, start_link/2, start_child/1, start_child/2]).
+
+%% gen_uploader callbacks
+-export([make_producer/3, make_uploader/1]).
+
+-include("workers_conf.hrl").
+
+%%%
+%%%  Public API
+%%%
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link(Config) -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link(Config) ->
+    start_link({local, ?make_server_name}, Config).
+
+start_link(Name, Config) ->
+    gen_uploader:start_link(Name, ?MODULE, Config, []).
+
+
+start_child(Config) ->
+    start_child(?make_server_name, Config).
+
+start_child(Name, Config) ->
+    ChildSpec = {Name,
+                 {?MODULE, start_link, [{local, Name}, Config]},
+                 temporary,
+                 brutal_kill,
+                 worker,
+                 [?MODULE]},
+    supervisor:start_child(?WORKERS_SUP, ChildSpec).
+
+%%%===================================================================
+%%% gen_uploader callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns the Key-Value producer function
+%%
+%% @spec make_producer(Seed, Quantity, ValueSize) ->
+%%                                 function/1 | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+make_producer(Seed, Quantity, ValueSize) ->
+    kv_producers:make_succeeding_kv_producer(Seed, Quantity, ValueSize).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns a function to perform actual Key-Value uploads
+%%
+%% @spec make_uploader(Connection) -> function/2 | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+make_uploader(_Connection) ->
+    fun put/2.
+
+
+%%
+%% Internal functions
+%%
+
+info_uploader(Connection) ->
+    fun (K,V) ->
+            Ks = lists:flatten(K),
+            Vs = lists:flatten(V),
+            error_logger:info_msg("info_uploader (~p connection):"
+                                  " ~p key, ~p value~n", [Connection, Ks, Vs]),
+            ok
+    end.
+
+put(K,V) ->
+%    httpc:set_options([{verbose,verbose}]),
+    ContentType = "text/plain",
+    RequestHeaders = [],
+    RequestBody = V,
+    RIAKNodeURL = "http://localhost:8091/riak/",
+    RIAKPutOptions = [],
+    Bucket = ?MODULE_STRING,
+    URL = lists:flatten([RIAKNodeURL, Bucket, "/", K, RIAKPutOptions]),
+    Request = {URL, RequestHeaders, ContentType, RequestBody},
+    {ok, {{_Version, 204, _ReasonPhrase}, _Headers, _Body}} =
+        httpc:request(put, Request, [], []),
+    ok.
