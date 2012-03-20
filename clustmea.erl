b2f3ad9bafc2ab2bf27f9e180156dd6be7ef7598
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2012-03-20 18:05:51 +0400

    Add restart function as extra API

--- clustmea.erl
+++ clustmea.erl
@@ -1,10 +1,21 @@
 -module(clustmea).
 -behaviour(application).
 
+%% application behaviour callbacks
 -export([start/2, stop/1]).
+
+%% extra API
+-export([restart/0]).
 
 start(_Type, _Args) ->
     clustmea_sup:start_link().
 
 stop(_State) ->
     ok.
+
+restart() ->
+    lists:map(
+      fun (F) ->
+              {F, application:F(?MODULE)}
+      end,
+      [stop, unload, start]).
